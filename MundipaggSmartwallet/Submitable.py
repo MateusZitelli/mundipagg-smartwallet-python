e2c4fcbbf1f5bdab2796cb4d942a8d3c8840d80f
Mateus Zitelli <zitellimateus@gmail.com>
2015-07-12 18:42:50 -0300

    Handle error responses

--- MundipaggSmartwallet/Submitable.py
+++ MundipaggSmartwallet/Submitable.py
@@ -1,26 +1,58 @@
 import json
 import requests
 import Utils
+import xml.etree.ElementTree as etree
+
+DataContract = "{http://schemas.datacontract.org/2004/07/SmartWalletService.DataContract}"
+
+NODES = {
+    "success": DataContract + 'Success',
+    "ErrorItem": DataContract + 'ErrorItem',
+    "Description": DataContract + 'Description',
+}
+
+
+class ErrorResponse(BaseException):
+    def __init__(self, descriptions):
+        self.descriptions = descriptions
 
 
 class Submitable:
     def __init__():
         pass
 
-    def toJSON(self, SmartWalletKey):
+    def toJSON(self, smartWalletKey):
         selfData = {k: v for k, v in self.__dict__.items() if v}
-        selfData['SmartWalletKey'] = SmartWalletKey
+        selfData['SmartWalletKey'] = smartWalletKey
         jsonData = json.dumps(selfData)
         return jsonData
 
-    def create(self, creationUrl, ResponseClass, smartWalletKey):
-        jsonData = self.toJSON(smartWalletKey)
-        Req = Utils.JsonRequest(creationUrl, jsonData)
-        response = None
+    def handleResponse(self, response):
         try:
-            response = Req.submit()
-        except requests.exceptions.HTTPError:
+            xmlTree = etree.fromstring(response.content)
+        except etree.ParseError:
             raise
 
+        if response.status_code != requests.codes.ok:
+            descriptions = []
+            errorItemPath = './/' + NODES["ErrorItem"]
+
+            for el in xmlTree.findall(errorItemPath):
+                descriptionNode = el.find(NODES["Description"])
+                descriptions.append(descriptionNode.text)
+
+            raise ErrorResponse(descriptions)
+
         responseDict = json.loads(response.json())
+        return responseDict
+
+    def create(self, creationUrl, ResponseClass, smartWalletKey):
+        jsonData = self.toJSON(smartWalletKey)
+
+        Req = Utils.JsonRequest(creationUrl, jsonData, header={
+            "SmarWalletKey": smartWalletKey
+        })
+
+        response = Req.submit()
+        resposeDict = self.handleResponse(response)
         return ResponseClass(*responseDict)
--- MundipaggSmartwallet/Utils.py
+++ MundipaggSmartwallet/Utils.py
@@ -2,14 +2,13 @@ import requests
 
 
 class JsonRequest:
-    def __init__(self, url, jsonData):
+    def __init__(self, url, jsonData, header):
         self.url = url
         self.jsonData = jsonData
+        self.header = {'Content-Type': 'application/json'}
+        self.header.update(header)
 
     def submit(self):
-        header = {'Content-Type': 'application/json'}
-        response = requests.post(self.url, data=self.jsonData, headers=header)
-        if response.status_code != requests.codes.ok:
-            response.raise_for_status()
-        else:
-            return response
+        response = requests.post(self.url, data=self.jsonData,
+                                 headers=self.header)
+        return response
